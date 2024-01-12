52b5ca127508301902d955c165da439b75aa6538
CodeX93 <85872799+CodeX93@users.noreply.github.com>
2024-01-12 15:52:57 +0500

    .

--- Desktop/ProjectF/backend/Routes/InsuranceRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const { getPlans } = require("../Controller/Insurance");
-
-router.get("/get-plans", getPlans);
-
-module.exports = router;
--- Desktop/ProjectF/backend/Routes/PaymentRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const { CreatePayment } = require("../Controller/Payment");
-
-router.post("/create-payment", CreatePayment);
-
-module.exports = router;
